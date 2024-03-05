c41dec2bc3e494871b32bd0ec918c3c069813de5
Polibeerex <polibeerex@gmail.com>
2024-03-05 05:00:53 +0100

    fixed port

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ const avatarStorage = multer.diskStorage({
 const uploadAvatar = multer({ storage: avatarStorage });
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // 404 middleware
 
